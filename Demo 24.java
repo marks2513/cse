9e768cabda15c17499349ac91912572ec8c1a869
BoddupallyRamyasri <ramyasriboddupally4@gmail.com>
2024-09-25 11:58:23 +0530

    Add files via upload

---
+++ Demo 24.java
@@ -0,0 +1,9 @@
+import java.lang:*;
+import java.io*;
+class Demo24
+{
+public static void main(string arg[])
+{
+system.out.or print("welcome to java program");
+}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
